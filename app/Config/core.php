cb6bb09fd218be640bd3b985c8d601cbff4ac5ac
Felipe <afmejia23@utp.edu.co>
2017-11-20 04:45:39 -0500

    Starter tutorial cakephp

--- app/Config/core.php
+++ app/Config/core.php
@@ -109,7 +109,7 @@
  * included primarily as a development convenience - and
  * thus not recommended for production applications.
  */
-	//Configure::write('App.baseUrl', env('SCRIPT_NAME'));
+	Configure::write('App.baseUrl', env('SCRIPT_NAME'));
 
 /**
  * To configure CakePHP to use a particular domain URL
@@ -232,12 +232,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUdfubWwvniR2G0FgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '76859309657453542909496749683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
